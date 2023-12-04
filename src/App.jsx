c2b4d5b6c5002bdddbb54d12a9f636c9016a1020
Magen22 <s22110402@student.unklab.ac.id>
2023-12-04 03:58:57 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Body from './assets/komponen/Body'
+import Footer from './assets/komponen/Footer'
+import Header from './assets/komponen/Header'
+import './assets/css/styles.css'
+import './assets/js/scripts'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Body />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/komponen/Body.jsx
@@ -0,0 +1,197 @@
+
+
+const Body = () => {
+  return (
+    <div>
+  {/* Navigation*/}
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a className="navbar-brand js-scroll-trigger" href="#page-top">
+      <span className="d-block d-lg-none">Magen Tengker</span>
+      <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="profile2.jpg" alt="..." /></span>
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
+    <div className="collapse navbar-collapse" id="navbarResponsive">
+      <ul className="navbar-nav">
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience" /></li>
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests" /></li>
+        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+      </ul>
+    </div>
+  </nav>
+  {/* Page Content*/}
+  <div className="container-fluid p-0">
+    {/* About*/}
+    <section className="resume-section" id="about">
+      <div className="resume-section-content">
+        <h1 className="mb-0">
+          Magen
+          <span className="text-primary">Tengker</span>
+        </h1>
+        <div className="subheading mb-5">
+          Minut,kec.kauditan,Treman · 081341035912 ·
+          <a href="mailto:name@email.com">Tengkerdvd@gmail.com</a>
+        </div>
+        <p className="lead mb-5">saya adalah mahasiswa semester 5 di UNKLAB</p>
+        <div className="social-icons">
+          <a className="social-icon" href="#!"><i className="fab fa-linkedin-in" /></a>
+          <a className="social-icon" href="#!"><i className="fab fa-github" /></a>
+          <a className="social-icon" href="#!"><i className="fab fa-twitter" /></a>
+          <a className="social-icon" href="#!"><i className="fab fa-facebook-f">https://www.facebook.com/magen.tengker</i></a>
+        </div>
+      </div>
+    </section>
+    <hr className="m-0" />
+    {/* Experience*/}
+    <section className="resume-section" id="experience">
+      <div className="resume-section-content">
+        <h2 className="mb-5" />
+        <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="flex-grow-1">
+            <h3 className="mb-0" />
+            <div className="subheading mb-3" />
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary" /></div>
+        </div>
+        <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="flex-grow-1">
+            <h3 className="mb-0" />
+            <div className="subheading mb-3" />
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary" /></div>
+        </div>
+        <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="flex-grow-1">
+            <h3 className="mb-0" />
+            <div className="subheading mb-3" />
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary" /></div>
+        </div>
+        <div className="d-flex flex-column flex-md-row justify-content-between">
+          <div className="flex-grow-1">
+            <h3 className="mb-0" />
+            <div className="subheading mb-3" />
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary" /></div>
+        </div>
+      </div>
+    </section>
+    <hr className="m-0" />
+    {/* Education*/}
+    <section className="resume-section" id="education">
+      <div className="resume-section-content">
+        <h2 className="mb-5">Education</h2>
+        <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="flex-grow-1">
+            <h3 className="mb-0">SMPn Treman</h3>
+            <div className="subheading mb-3" />
+            <div />
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary">August 2015 - May 2018</span></div>
+        </div>
+        <div className="d-flex flex-column flex-md-row justify-content-between">
+          <div className="flex-grow-1">
+            <h3 className="mb-0">SMAk EBEN HAEZER MANADO</h3>
+            <div className="subheading mb-3">ipa</div>
+            <p />
+          </div>
+          <div className="flex-shrink-0"><span className="text-primary">August 2018 - May 2021</span></div>
+        </div>
+      </div>
+    </section>
+    <hr className="m-0" />
+    {/* Skills*/}
+    <section className="resume-section" id="skills">
+      <div className="resume-section-content">
+        <h2 className="mb-5">Skills</h2>
+        <div className="subheading mb-3">Programming Languages &amp; Tools</div>
+        <ul className="list-inline dev-icons">
+          <li className="list-inline-item"><i className="fab fa-html5" /></li>
+          <li className="list-inline-item"><i className="fab fa-css3-alt" /></li>
+          <li className="list-inline-item"><i className="fab fa-js-square" /></li>
+          <li className="list-inline-item"><i className="fab fa-angular" /></li>
+          <li className="list-inline-item"><i className="fab fa-react" /></li>
+          <li className="list-inline-item"><i className="fab fa-node-js" /></li>
+          <li className="list-inline-item"><i className="fab fa-sass" /></li>
+          <li className="list-inline-item"><i className="fab fa-less" /></li>
+          <li className="list-inline-item"><i className="fab fa-wordpress" /></li>
+          <li className="list-inline-item"><i className="fab fa-gulp" /></li>
+          <li className="list-inline-item"><i className="fab fa-grunt" /></li>
+          <li className="list-inline-item"><i className="fab fa-npm" /></li>
+        </ul>
+        <div className="subheading mb-3">Workflow</div>
+        <ul className="fa-ul mb-0">
+          <li>
+            <span className="fa-li"><i className="fas fa-check" /></span>
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-check" /></span>
+          </li><li>
+            <span className="fa-li"><i className="fas fa-check" /></span>
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-check" /></span>
+            A
+          </li>
+        </ul>
+      </div>
+    </section>
+    <hr className="m-0" />
+    {/* Interests*/}
+    <section className="resume-section" id="interests">
+      <div className="resume-section-content">
+        <h2 className="mb-5" />
+        <p />
+        <p className="mb-0" />
+      </div>
+    </section>
+    <hr className="m-0" />
+    {/* Awards*/}
+    <section className="resume-section" id="awards">
+      <div className="resume-section-content">
+        <h2 className="mb-5">Awards &amp; Certifications</h2>
+        <ul className="fa-ul mb-0">
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            basic javascript Certification
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            <sup />
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            <sup />
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            <sup />
+          </li>
+          <li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            <sup />
+          </li><li>
+            <span className="fa-li"><i className="fas fa-trophy text-warning" /></span>
+            <sup />
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</div>
+
+    
+  )
+}
+
+export default Body
